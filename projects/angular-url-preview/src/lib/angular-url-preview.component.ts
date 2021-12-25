e587b90bed93dc14a9a377f2be76df1bf772974d
Damjan <damjandimitrov1@gmail.com>
2021-12-25 22:23:46 +0100

    Implement URL Preview component and module

--- projects/angular-url-preview/src/lib/angular-url-preview.component.ts
+++ projects/angular-url-preview/src/lib/angular-url-preview.component.ts
@@ -1,20 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { URLMetadata } from './types/url-metadata';
 
 @Component({
   selector: 'ngx-url-preview',
-  template: `
-    <p>
-      angular-url-preview works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './angular-url-preview.component.html',
+  styleUrls: ['./angular-url-preview.component.scss']
 })
 export class AngularUrlPreviewComponent implements OnInit {
+  @Input() url!: string;
+  metadata!: URLMetadata;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    if (!this.url) throw Error('URL is required!');
+
+    type metadataResult = { metadata: URLMetadata };
+
+    this.http.get(`https://rlp-proxy.herokuapp.com/v2?url=${this.url}`)
+      .subscribe(result => {
+        const data = result as metadataResult;
+        this.metadata = new URLMetadata(data.metadata);
+        // Get the second-to-last segment of the host name
+        const siteName = this.metadata.hostname.split('.').splice(-2)[0];
+        // Assign capitalized site name to metadata if it is not set
+        this.metadata.siteName ||= siteName[0].toUpperCase() + siteName.substring(1).toLowerCase();
+      });
+  }
+
+  navigateToUrl() {
+    window.open(this.metadata.url, '_blank');
   }
 
 }
--- projects/angular-url-preview/src/lib/angular-url-preview.module.ts
+++ projects/angular-url-preview/src/lib/angular-url-preview.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { AngularUrlPreviewComponent } from './angular-url-preview.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [
-    AngularUrlPreviewComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    AngularUrlPreviewComponent
-  ]
+  declarations: [AngularUrlPreviewComponent],
+  imports: [BrowserModule, HttpClientModule],
+  exports: [AngularUrlPreviewComponent]
 })
 export class AngularUrlPreviewModule { }
